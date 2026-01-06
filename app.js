15697c7e629d88c90e04373d450303c314ab2f3a
Karthick Tamilselvam <developerkarthick1@gmail.com>
2026-01-06 13:42:10 +0530

    Add.js,app.test.js: Change in Message

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const name ="T Karthick";
 app.get("/", (req, res) => {
-  res.send("Welcome to APPLES Youtube Channel @cranky479 Please Subscribe By, T Karthick");
+  res.send(`Welcome to APPLES Youtube Channel @cranky479 Please Subscribe By, ${name}`);
 });
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = {app,server}
+module.exports = {app,server,name}
