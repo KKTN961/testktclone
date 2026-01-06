6e3789ce6fa04631180f7bb4092cd91238b00549
Karthick Tamilselvam <developerkarthick1@gmail.com>
2026-01-06 12:40:35 +0530

    previous commit Add CI Pipeline with Github Actions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to APPLES Youtube Channel @cranky479 Please Subscribe By, --> T Karthick");
+  res.send("Welcome to APPLES Youtube Channel @cranky479 Please Subscribe By, T Karthick");
 });
 
 const server = app.listen(PORT, () => {
--- test/app.test.js
+++ test/app.test.js
@@ -8,7 +8,7 @@ const {app,server} = require('../app')
     it("should return 200 status and the correct message",async() => {
        const response = await request(app).get("/");
        expect(response.status).toBe(200);
-       expect(response.text).toBe("Welcome to APPLES Youtube Channel @cranky479 Please Subscribe By, --> T Karthick");
+       expect(response.text).toBe("Welcome to APPLES Youtube Channel @cranky479 Please Subscribe By, T Karthick");
     });
 });
 
